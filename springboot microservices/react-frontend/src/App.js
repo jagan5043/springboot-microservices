d471fab934b2f0f0873daeed461dafa7f4d5a2ec
Your Name <you@example.com>
2023-09-26 00:24:15 +0530

    react completed

--- springboot microservices/react-frontend/src/App.js
+++ springboot microservices/react-frontend/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import EmployeeComponent from './component/EmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Hello World!
-      </header>
+    <div className="container">
+        <EmployeeComponent/>
     </div>
   );
 }
